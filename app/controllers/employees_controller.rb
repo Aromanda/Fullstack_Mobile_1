da8716c73c5bf39b3dc29bd2d17a2cea6ee795f7
Aromanda <aromandanyfitiaratoejanahary@yahoo.fr>
2023-08-07 15:52:50 -0400

    add skip before action in employee controller

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,5 +1,6 @@
 class EmployeesController < ApplicationController
   before_action only: [:show, :edit, :update, :destroy]
+  skip_before_action :verify_authenticity_token
 
   def index
     @employees = Employee.all  
