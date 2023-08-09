0267252f1f83ca30e1b6a5426d97e6b8fc38ac61
Aromanda <aromandanyfitiaratoejanahary@yahoo.fr>
2023-08-09 18:36:00 -0400

    change Ngrok code

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
 
-  config.hosts << "6026-144-172-139-35.ngrok-free.app"
+  config.hosts << "dc85-144-172-139-35.ngrok-free.app"
 
   # Settings specified here will take precedence over those in config/application.rb.
 
